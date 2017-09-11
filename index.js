249da1c38af130036ae26e78b8632e66399b641d
Yes <joshuayes@gmail.com>
2017-09-12 02:59:42 +0800

    analyer

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ const geo = require("node-geo-distance");
 // const parseString = require('xml2js').parseString;
 
 const gpx = new DOMParser().parseFromString(
-  fs.readFileSync("./test.gpx", "utf8")
+  fs.readFileSync("./shui-forest.gpx", "utf8")
 );
 
 const trail = tj.gpx(gpx);
 const coordTimes = trail.features[0].properties.coordTimes;
-console.log("conv: ", trail.features[0].properties.coordTimes.length);
-console.log("conv: ", trail.features[0].geometry.coordinates.length);
+// console.log("conv: ", trail.features[0].properties.coordTimes.length);
+// console.log("conv: ", trail.features[0].geometry.coordinates.length);
 
 const duration = moment(_.last(coordTimes)).diff(
   moment(_.first(coordTimes)),
@@ -34,6 +34,7 @@ const target = {
   latitude: 23.6011326,
   longitude: 120.789729
 };
+
 let td = 0;
 let tdh = 0;
 const cc = trail.features[0].geometry.coordinates;
@@ -48,18 +49,91 @@ for (var i = 0; i < cc.length - 1; i++) {
   };
   tdh += +geo.haversineSync(coord1, coord2);
   td += +geo.vincentySync(coord1, coord2);
-  if (+geo.vincentySync(coord1, target) < 20) {
-    console.log("yes: ", i, coord1, geo.vincentySync(coord2, target));
-  }
+  // if (+geo.vincentySync(coord1, target) < 20) {
+  //   console.log("yes: ", i, coord1, geo.vincentySync(coord2, target));
+  // }
 }
 // var vincentyDist = geo.vincentySync(coord1, coord2);
 // console.log('vv: ', vincentyDist)
 console.log("td: ", td);
 console.log("td: ", tdh);
-console.log("l ", cc.length);
-
+// console.log("l ", cc.length);
 // console.log(util.inspect(converted, false, null))
-
 // console.log('conv: ', convertedWithStyles)
-
 // parseString(fs.readFileSync('./test.gpx', 'utf8'))
+
+const nodes = [
+  { lat: 23.6260045, lng: 120.7991155 }, // 仁亭
+  { lat: 23.6211228, lng: 120.796533 }, // 鹿屈山登山口
+  { lat: 23.5965233, lng: 120.8017566 }, // 岔路
+  { lat: 23.5830092, lng: 120.8026576 } // 水漾森林
+  // { lat: 23.6156614, lng: 120.7875484 }, // 鹿屈山前鋒
+  // { lat: 23.6011326, lng: 120.789729 }, // 鹿屈山
+  // { lat: 23.6156614, lng: 120.7875484 }, // 鹿屈山前鋒
+  // { lat: 23.6211228, lng: 120.796533 }, // 鹿屈山登山口
+  // { lat: 23.6260045, lng: 120.7991155 } // 仁亭
+];
+
+// function findStartAndEnd(node, points) {
+//   let result = [];
+//   const coord2 = { latitude: node.lat, longitude: node.lng };
+//   for (var i = 0; i < points.length - 1; i++) {
+//     const coord1 = { latitude: points[i][1], longitude: points[i][0] };
+//     const d = +geo.vincentySync(coord1, coord2);
+//     if (result.length === 0 && d < 20) {
+//       result.push({ i, lat: points[i][1], lng: points[i][0] });
+//     }
+//     if (result.length === 1 && d > 20) {
+//       result.push({ i, lat: points[i - 1][1], lng: points[i - 1][0] });
+//     }
+//   }
+//   return result;
+// }
+
+// const startAndEnds = nodes.map(n =>
+//   findStartAndEnd(n, trail.features[0].geometry.coordinates)
+// );
+
+// let times = [];
+// for (var i = 0; i < startAndEnds.length - 1; i++) {
+//   const endTime = trail.features[0].properties.coordTimes[startAndEnds[i][1].i];
+//   const startTime =
+//     trail.features[0].properties.coordTimes[startAndEnds[i + 1][0].i];
+//   times.push(moment(startTime).diff(moment(endTime), "s"));
+// }
+
+function analyize(nodes, points) {
+  let result = [];
+  let j = 0;
+  let coord2 = { latitude: nodes[j].lat, longitude: nodes[j].lng };
+  for (var i = 0; i < points.length - 1; i++) {
+    const coord1 = { latitude: points[i][1], longitude: points[i][0] };
+    const d = +geo.vincentySync(coord1, coord2);
+    if (result.length % 2 === 0 && d < 30) {
+      console.log(coord1);
+      result.push(i);
+      continue;
+    }
+    if (result.length % 2 === 1 && d > 30) {
+      console.log(coord1);
+      result.push(i);
+      if (j < nodes.length - 1) {
+        j++;
+        coord2 = { latitude: nodes[j].lat, longitude: nodes[j].lng };
+      } else {
+        break;
+      }
+    }
+  }
+  return result;
+}
+
+const result = analyize(nodes, trail.features[0].geometry.coordinates);
+let timesg = [];
+for (var i = 1; i < result.length - 1; i += 2) {
+  const endTime = trail.features[0].properties.coordTimes[result[i]];
+  const startTime = trail.features[0].properties.coordTimes[result[i + 1]];
+  timesg.push(moment(startTime).diff(moment(endTime), "s"));
+}
+console.log(result);
+console.log(timesg.map(t => numeral(t).format("00:00:00")));
